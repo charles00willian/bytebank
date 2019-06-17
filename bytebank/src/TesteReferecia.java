661874c9c9bd716cdde62e4b019a3612cf363e4d
Charles Willian <wcharlesw15@gmail.com>
2019-06-17 17:39:33 -0300

    first commit

---
+++ bytebank/src/TesteReferecia.java
@@ -0,0 +1,14 @@
+
+public class TesteReferecia {
+	public static void main(String[] args) {
+		Conta primeiraConta = new Conta();
+		primeiraConta.saldo = 300;
+		
+		System.out.println("Saldo da primeira: " + primeiraConta.saldo);
+		
+		Conta segundaConta = primeiraConta;
+		
+		System.out.println("Saldo da segunda conta: " + segundaConta.saldo);
+		
+	}
+}
